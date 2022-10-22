de481ecfe5d6d8b1fd6cefdaf2a91d7937d90160
Chukov97 <ilyachuk@gmail.com>
2022-10-23 00:49:16 +0300

    add nested loops

---
+++ nested_loops/nested_loops.py
@@ -0,0 +1,16 @@
+for number in range(10):
+    count = 0
+    emoticons = ''
+    while count <= number:
+        emoticons += '\U0001f600'
+        count += 1
+    print(emoticons)
+
+for number in range(10):
+    emoticons = ''
+    for count in range(number + 1):
+        emoticons += '\U0001f600'
+    print(emoticons)
+
+for number in range(1, 11):
+    print('\U0001f600' * number)
